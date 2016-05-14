8757e777df74be10f36e7e2555c8b643928b6ad1
Alex B <arboerstler@gmail.com>
2016-05-14 14:59:59 -0400

    adds word-chain fn and test

--- src/markov_tolkien/generator.clj
+++ src/markov_tolkien/generator.clj
@@ -10,8 +10,9 @@ words
 (def word-transitions (partition-all 3 1 words))
 word-transitions
 
-(reduce (fn [r t] (merge-with clojure.set/union r
-                             (let [[a b c] t]
-                               {[a b] (if c #{c} #{})})))
-        {}
-        word-transitions)
+(defn word-chain [word-transitions]
+  (reduce (fn [r t] (merge-with clojure.set/union r
+                               (let [[a b c] t]
+                                 {[a b] (if c #{c} #{})})))
+          {}
+          word-transitions))
--- test/markov_tolkien/generator_test.clj
+++ test/markov_tolkien/generator_test.clj
@@ -2,6 +2,13 @@
   (:require [clojure.test :refer :all]
             [markov-tolkien.generator :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-word-chain
+  (testing "it produces a chain of the possible two step transitions between suffixes and prefixes"
+    (let [example '(("And" "the" "Golden")
+                    ("the" "Golden" "Grouse")
+                    ("And" "the" "Pobble")
+                    ("the" "Pobble" "who"))]
+      (is (= {["the" "Pobble"] #{"who"}
+              ["the" "Golden"] #{"Grouse"}
+              ["And" "the"] #{"Pobble" "Golden"}}
+             (word-chain example))))))
